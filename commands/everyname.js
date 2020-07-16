bb31f6bdb12f0da98c34c24562c6a70e7bcf2ce1
SeanKohler <57458971+SeanKohler@users.noreply.github.com>
2020-07-16 12:59:00 -0400

    📋 Removed URL embed 📋
    If someone requested a direct URL when displaying the names, the URL creates an embed for the video in chat. This removes those embeds

--- commands/everyname.js
+++ commands/everyname.js
@@ -1,10 +1,17 @@
 module.exports = {
     name: 'everyname',
     description: 'This command is a shorthand version of history command. It only shows the names of previously played songs and not any other data',
-    execute(message, args){
+    execute(message, cache){
         message.channel.bulkDelete(1);
         var namestr="";
         for(var i=0; i<cache.name.length; i++){
+            if(cache.name[i].includes("https://")){//If a url is detected add < & > on either side so it wont create an embed
+                if(cache.name[i].includes("<")){
+                    //Already added to ends do nothing
+                }else{
+                   cache.name[i]="<"+cache.name[i]+">"; 
+                }
+            }
             namestr+=cache.name[i]+", ";
         }
         message.channel.send(namestr);
